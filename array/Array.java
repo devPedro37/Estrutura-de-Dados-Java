36ec8e390fb634c0f9eb098dc6e783118c249a7d
Pedro Lucas de Sousa Ribeiro <pedro.ribeiro@tbmtextil.com.br>
2024-06-24 12:51:32 -0300

    Refatorando o metodo de adicionar. Melhorando a forma de inserir trazendo mais velocidade na execucao

--- array/Array.java
+++ array/Array.java
@@ -9,20 +9,24 @@ public class Array {
 
     //INSTANCIAÇÃO DO ARRAY DE ALUNOS
     //ESTRUTURA: tipo, declaração dos colchetes, nomeArray, criação do objeto e tamanhoArray
-    private Aluno[] alunos = new Aluno[100];
-
+    private Aluno[] Vetoralunos = new Aluno[100];
+    private int totalDeAlunos = 0;
 
     //IMPLEMENTAÇÃO DAS AÇÕES DA CLASSE
 
     //ADICIONAR ALUNO
     public void adicionarAluno(Aluno aluno) {
-        for(int i = 0; i < alunos.length; i++){
-            if(alunos[i] == null){
+       /* for(int i = 0; i < alunos.length; i++){ ESTA FORMA DE ADICIONAR ALUNO TEM UM ERRO. NA PRIMEIRA VEZ QUE ESTOU EXECUTANDO O LAÇO ELE VAI ENCONTRAR UMA POSIÇÃO VAZIA, MAS E SE EU
+            if(alunos[i] == null){                         QUISER ADICIONAR UM ALUNO E A POSIÇÃO DISPONÍVEL FOR A POSIÇÃO 71? ELE IRÁ PERCORRER O LAÇO 70 VEZES PARA ADICIONAR TORNANDO O ALGORITMO LENTO
                 alunos[i] = aluno;
                 break;
             }
         }
-    }
+        */
+        //NOVA IMPLEMENTAÇÃO
+        this.Vetoralunos[totalDeAlunos] = aluno; //É CRIADO UMA VARIÁVEL QUE GUARDA A QUANTIDADE DE ALUNOS E É INICIADA COM 0. DENTRO DO ADICIONAR, É CHAMADO O VETOR DE ALUNOS E PASSAMOS
+        totalDeAlunos++;                                    //E PASSAMOS A POSIÇÃO QUE INDICA QUANTOS ALUNOS TEM CADASTRADO NO ARRAY E A PRÓXIMA POSIÇÃO VAZIA. QUANDO ADICIONAMOS UM ALUNO ELE GUARDA
+    }                                                                  // NA VARIÁVEL A POSIÇÃO DO OBJETO ALUNO, INCREMENTA E APONTA O ESPAÇO VAZIO.
 
     //PEGAR UM ALUNO DO ARRAY
     public Aluno posicaoAluno(int posicao) {
@@ -49,6 +53,6 @@ public class Array {
     //TOSTRING DO ARRAY
     //PEGA TODOS OS ELEMENTOS DO ARRAY E CHAMA O TOSTRING PARA TODOS ELES
     public String toString() {
-        return Arrays.toString(alunos);
+        return Arrays.toString(Vetoralunos);
     }
 }
